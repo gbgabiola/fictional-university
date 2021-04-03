54ef4a8b56d7fed37d81cba997dc969470ac3320
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2021-04-03 18:04:31 +0800

    Update all files

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>);"></div>
   <div class="page-banner__content container t-center c-white">
     <h1 class="headline headline--large">Welcome!</h1>
     <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
@@ -46,11 +46,12 @@
       $homepagePosts = new WP_Query(array(
         'posts_per_page' => 2
       ));
+
       while ($homepagePosts->have_posts()) {
         $homepagePosts->the_post(); ?>
         <div class="event-summary">
           <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php the_time('M'); ?></span>
+            <span class="event-summary__month"><?php the_time('M') ?></span>
             <span class="event-summary__day"><?php the_time('d'); ?></span>
           </a>
           <div class="event-summary__content">
@@ -70,7 +71,7 @@
 <div class="hero-slider">
   <div data-glide-el="track" class="glide__track">
     <div class="glide__slides">
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg') ?>);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bus.jpg'); ?>);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -79,7 +80,7 @@
           </div>
         </div>
       </div>
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg') ?>);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/apples.jpg'); ?>);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">An Apple a Day</h2>
@@ -88,7 +89,7 @@
           </div>
         </div>
       </div>
-      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg') ?>);">
+      <div class="hero-slider__slide" style="background-image: url(<?php echo get_theme_file_uri('/images/bread.jpg'); ?>);">
         <div class="hero-slider__interior container">
           <div class="hero-slider__overlay">
             <h2 class="headline headline--medium t-center">Free Food</h2>
@@ -102,4 +103,4 @@
   </div>
 </div>
 
-<? get_footer(); ?>
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">Welcome to our blog!</h1>
     <div class="page-banner__intro">
@@ -15,7 +15,7 @@
   while (have_posts()) {
     the_post(); ?>
     <div class="post-item">
-      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <h2 class="headline headline--medium headling--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <div class="metabox">
         <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
       </div>
@@ -25,6 +25,7 @@
       </div>
     </div>
   <?php }
+
   echo paginate_links();
   ?>
 </div>
