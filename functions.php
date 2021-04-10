15efed845eff8e02dd83f0b35d38e7de58695654
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2021-04-10 17:28:33 +0800

    Remove bundled assets

--- functions.php
+++ functions.php
@@ -4,12 +4,12 @@ function university_files() {
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
-  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
+  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.ddev.site')) {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
-    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.31439193d4ec99f003a1.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.0c6b0d5dc96d0ff7bc2e.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.0c6b0d5dc96d0ff7bc2e.css'));
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.css'));
   }
 }
 
